9bc20615a07c023aaf538c372d81ee8555af081a
Roberto Nogueira <enogrob@gmail.com>
2017-10-24 11:52:26 -0200

    performing-chapter-3-model-specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with a first name, last name and email, and password"
+  it "is invalid without a first name"
+  it "is invalid without a last name"
+  it "is invalid without an email address"
+  it "is invalid with a duplicate email address"
+  it "returns a user's full name as a string"
+end
